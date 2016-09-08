ddb8f45ef4c104de75828144c08f8c3da9ced734
Taniraw <tanirawrites@gmail.com>
2016-09-08 15:19:25 -0400

    Array Challenge Lab Completed

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -14,28 +14,41 @@ class ViewController: UIViewController {
     var shoppingList: [String] = []
     
     // Quantity of items
-    var quantityOfItems: [Int] = [6, 4, 12, 4]
+    var arrayOfQuantities: [Int] = [6, 4, 12, 4]
     
     // Name of items
-    var nameOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
+    var arrayOfItems: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
     
     
-    override func viewDidLoad() {
+    
+    func makeShoppingListWithItems(arrayOfItems: [String] , quantityOfItems: [Int])-> [String] {
+        let shoppingList : [String] = []
+        for (index, item) in arrayOfItems.enumerate(){
+           print("\(index+1). \(item) \(arrayOfQuantities[index])")
+            }
+        return shoppingList
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override  func viewDidLoad() {
         super.viewDidLoad()
+    shoppingList =  makeShoppingListWithItems(arrayOfItems, quantityOfItems: arrayOfQuantities)
+        
         
         // After you're done implementing your method, call on it here. You can remove this example below
-        
-        thisIsAnExample()
-        
     }
-    
-    // This is just an example - you can remove this.
-    func thisIsAnExample() {
-        print("This ist just an example, you can remvove this.")
-    }
-    
-    
-    // Implement your method here
-
-    
 }
+
+
+            
+            
+
